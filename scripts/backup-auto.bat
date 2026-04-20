35c40c698e9c8160423aa3d0c47afb6e1fd6d1e0
Marco Amato <panichelli.info@gmail.com>
2026-04-20 11:15:07 +0200

    fix backup

--- scripts/backup-auto.bat
+++ scripts/backup-auto.bat
@@ -1,4 +1,3 @@
 @echo off
 cd /d "C:\Users\maram\OneDrive\Desktop\ gestionale"
-"C:\Program Files\nodejs\node.exe" "C:\Users\maram\OneDrive\Desktop\ gestionale\scripts\backup-auto.mjs"
-pause
+"C:\Program Files\nodejs\node.exe" "C:\Users\maram\OneDrive\Desktop\ gestionale\scripts\backup-auto.mjs" %*
